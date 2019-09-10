902dba0b8e81cc987c3279aec1b9da6c41cfea87
ljl <1510835092@qq.com>
2019-09-10 19:59:49 +0800

    first

---
+++ muxure/muxingService/src/main/java/com/muxing/muxing/MuxingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muxing.muxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuxingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuxingServiceApplication.class, args);
+    }
+
+}
---
+++ muxure/muxingService/src/test/java/com/muxing/muxing/MuxingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muxing.muxing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuxingServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
